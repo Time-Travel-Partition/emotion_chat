9c2dc3b9287b76d1b2f23b9d5eb30fb94111ad7f
okxooxoo <okxooxoo@naver.com>
2024-03-14 19:15:40 +0900

    ✨ feat: dart_openai 연동 및 어시스턴트 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:dart_openai/dart_openai.dart';
+import 'package:emotion_chat/env/env.dart';
 import 'package:emotion_chat/services/auth/auth_gate.dart';
 import 'package:emotion_chat/services/auth/auth_service.dart';
 import 'package:emotion_chat/themes/light_mode.dart';
@@ -7,6 +11,43 @@ import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  OpenAI.apiKey = Env.apiKey;
+
+  // Assistant에게 대화의 방향성을 알려주는 메시지
+  final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+    content: [
+      OpenAIChatCompletionChoiceMessageContentItemModel.text(
+        "You're a psychological consultant.",
+      ),
+    ],
+    role: OpenAIChatMessageRole.system,
+  );
+
+  // 사용자가 보내는 메시지
+  final userMessage = OpenAIChatCompletionChoiceMessageModel(
+    content: [
+      OpenAIChatCompletionChoiceMessageContentItemModel.text(
+        '안녕? 나는 조금 우울해. :(',
+      ),
+    ],
+    role: OpenAIChatMessageRole.user,
+  );
+
+  final requestMessages = [
+    systemMessage,
+    userMessage,
+  ];
+
+  OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
+    model: 'gpt-3.5-turbo',
+    messages: requestMessages,
+    maxTokens: 250,
+  );
+
+  log(chatCompletion.choices.first.message.toString());
+  log(chatCompletion.systemFingerprint.toString());
+  log(chatCompletion.usage.promptTokens.toString());
+  log(chatCompletion.id.toString());
   //firestore 초기화
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
