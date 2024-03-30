e23cb6aacb4ff0a22c11d29c7be1b14f74fbe45d
okxooxoo <okxooxoo@naver.com>
2024-03-30 22:26:56 +0900

    ✨ feat: 프롬프트 튜닝하여 대화의 방향성 조정

--- lib/services/openai/openai_service.dart
+++ lib/services/openai/openai_service.dart
@@ -1,6 +1,5 @@
 import 'package:dart_openai/dart_openai.dart';
 import 'package:emotion_chat/env/env.dart';
-import 'package:emotion_chat/screens/main/chat_tab/chat_screen.dart';
 import 'package:emotion_chat/services/chat/chat_service.dart';
 
 class OpenAIService {
@@ -10,33 +9,39 @@ class OpenAIService {
   Future<String> createModel(String sendMessage, int emotion) async {
     OpenAI.apiKey = Env.apiKey;
     OpenAI.requestsTimeOut = const Duration(seconds: 200); // 시간 제한 늘림
-    String emotionToString = getEmotionString(emotion);
+
+    String emotionString = getEmotionString(emotion);
+
     // Assistant에게 대화의 방향성을 알려주는 메시지
     final systemSettingMessage = OpenAIChatCompletionChoiceMessageModel(
       content: [
         OpenAIChatCompletionChoiceMessageContentItemModel.text(
-          '너는 심리 상담가야. 내담자는 $emotionToString의 감정을 느꼈어. 내담자가 자신의 감정을 잘 이해할 수 있도록 도와줘. 조언보다 공감을 해줘. 질문을 통해 내담자의 대답을 이끌어 내는 것도 좋은 방법이야.',
+          // 명확하고 구체적으로 적어야 함.
+          // ex) 친근하게 다가가줘 (X) 이름을 부르거나 스몰토크를 해줘 (O)
+          // 꼭 필요한 내용만 적어야 함.
+          '''
+            한국어를 사용하면서 높임말로 대답해줘.
+            두 문장 안으로 최대한 짧게 대답해줘.
+            너는 심리 상담가야. 내담자는 $emotionString의 감정을 느꼈어.
+            내담자의 이름을 부르거나 스몰토크를 하면서 친근하게 다가가줘.
+            내담자가 감정에 대한 생각과 상황을 분리해서 생각하도록 도와줘.
+            내담자가 감정의 원인을 찾도록 대화를 유도하고 너는 적절하게 맞장구를 쳐줘.
+            지나치게 낙관적인 대답은 지양해줘.
+            내담자의 감정의 크기가 큰 경우,
+            다른 이유나 삽화적 기억이 있는지를 대화를 통해 확인하고 전문가와의 상담을 제안해줘.
+            감정의 크기가 작은 경우,
+            그 대상과 관련된 에피소드를 계속 말하게 하고 공감함으로써 조금씩 감정을 완화시켜줘.
+          ''',
         ),
       ],
       role: OpenAIChatMessageRole.system,
     );
+
     if (messagesList.isEmpty) {
       messagesList.add(systemSettingMessage);
+      // 이전 메시지 내역 추가
       await _addMessagesFromFirestore(emotion);
     }
-    // // conversationHistory에 있는 메시지들을 messagesList에 추가
-    // for (String historyMessage in conversationHistory) {
-    //   final historyMessageModel = OpenAIChatCompletionChoiceMessageModel(
-    //     content: [
-    //       OpenAIChatCompletionChoiceMessageContentItemModel.text(
-    //         historyMessage,
-    //       ),
-    //     ],
-    //     role: OpenAIChatMessageRole.user,
-    //   );
-
-    //   messagesList.add(historyMessageModel);
-    // }
 
     // 사용자가 보내는 메시지
     final userMessage = OpenAIChatCompletionChoiceMessageModel(
@@ -50,26 +55,28 @@ class OpenAIService {
 
     messagesList.add(userMessage);
 
-    // 메시지 리스트 출력
     final requestMessages = [...messagesList];
     OpenAIChatCompletionModel chatCompletion =
         await OpenAI.instance.chat.create(
       model: 'gpt-3.5-turbo',
       messages: requestMessages,
       maxTokens: 250,
+      temperature: 1.0,
     );
 
     String message =
         chatCompletion.choices.first.message.content![0].text.toString();
 
-    // 시스템 응답을 messageList에 추가
+    // 시스템 응답을 messageList에 추가 (DB에도 넣어야 할 듯)
     final systemMessage = OpenAIChatCompletionChoiceMessageModel(
       content: [
         OpenAIChatCompletionChoiceMessageContentItemModel.text(message),
       ],
-      role: OpenAIChatMessageRole.system,
+      role: OpenAIChatMessageRole.assistant,
     );
+
     messagesList.add(systemMessage);
+
     return message;
   }
 
@@ -77,8 +84,7 @@ class OpenAIService {
     await for (var snapshot in _chatService.getMessages(emotion)) {
       for (var document in snapshot.docs) {
         final messageData = document.data() as Map<String, dynamic>;
-        final messageContent = messageData['content']
-            as String; // 'content'는 Firestore에서 메시지 내용을 저장하는 필드의 이름입니다.
+        final messageContent = messageData['content'] as String;
 
         final messageModel = OpenAIChatCompletionChoiceMessageModel(
           content: [
@@ -86,13 +92,13 @@ class OpenAIService {
                 messageContent),
           ],
           role: messageData['isBot'] == true
-              ? OpenAIChatMessageRole.system
+              ? OpenAIChatMessageRole.assistant
               : OpenAIChatMessageRole.user,
         );
 
         messagesList.add(messageModel);
       }
-      break; // 이 예시에서는 스트림의 첫 번째 QuerySnapshot만 처리합니다. 필요에 따라 로직을 조정하세요.
+      break; // 스트림의 첫 번째 QuerySnapshot만 처리
     }
   }
 
